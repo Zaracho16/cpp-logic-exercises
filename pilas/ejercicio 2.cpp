406779c2f79b041f3dc6b1c7f9c96960466fbd29
Adrian Zaracho <adrianzaracho16@gmail.com>
2025-09-28 22:56:56 -0300

    push ej2

---
+++ pilas/ejercicio 2.cpp
@@ -0,0 +1,34 @@
+
+// 1) Insertar y mostrar elementos
+// Crea un programa que permita al usuario ingresar 5 números y los guarde en una pila. 
+// Luego, muestra los elementos en el orden en que se van desapilando.
+
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main() {
+	
+	stack<int> pila;
+	int valorAgregado;
+	
+	cout << "Ingrese un valor para la pila: ";
+	cin >> valorAgregado;
+	pila.push(valorAgregado);
+	
+	for(int i=0; i<4; i++) {
+		cout << "Ingrese otro valor para la pila: ";
+		cin >> valorAgregado;
+		pila.push(valorAgregado);
+	}
+	
+	cout << "\nPila insertada: " << endl;
+	while(!pila.empty()) {	
+		cout << "|" << pila.top() << "|" << endl;
+		pila.pop();
+	}
+	
+	return 0;
+	
+}
